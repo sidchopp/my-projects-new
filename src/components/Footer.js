76061e1aa80a5f146ab805ffedcb17fc0e9c1408
sidchopp <sidchopp@gmail.com>
2022-08-01 12:57:00 -0600

    maple leaf icon in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,81 +1,51 @@
-
-import Typography from '@material-ui/core/Typography';
-import { Icon } from 'semantic-ui-react';
-import IconButton from '@mui/material/IconButton';
+import Typography from "@material-ui/core/Typography";
+import { Icon } from "semantic-ui-react";
+import IconButton from "@mui/material/IconButton";
+import { FaCanadianMapleLeaf } from "react-icons/fa";
 
 // Components
-import ContactMe from './ContactMe';
-import UseStyles from './UseStyles';
-import Today from './Today';
-
+import ContactMe from "./ContactMe";
+import UseStyles from "./UseStyles";
+import Today from "./Today";
 
 // Amimate.css
-import "animate.css"
+import "animate.css";
 
 function Footer() {
   const classes = UseStyles();
   return (
     <footer className={classes.footer}>
-      <Typography
-        variant="subtitle1"
-        align="center"
-        component="p"
-        gutterBottom
-      >
-        <span className='font'>Hope you liked my work. </span>
+      <Typography variant="subtitle1" align="center" component="p" gutterBottom>
+        <span className="font">Hope you liked my work. </span>
       </Typography>
-      <Typography
-        variant="subtitle1"
-        align="center"
-        component="p"
-      >
-        <Icon
-          name='mobile alternate'
-          fitted
-        /> <span className='font'> Wanna Talk? Dial (587)-436-7497</span>
+      <Typography variant="subtitle1" align="center" component="p">
+        <Icon name="mobile alternate" fitted />{" "}
+        <span className="font"> Wanna Talk? Dial (587)-436-7497</span>
       </Typography>
-      <Typography
-        variant="subtitle1"
-        align="center"
-        component="p"
-      >
-        <span className='font'>Not a big talker? Drop a</span>
-        {" "}
-        <IconButton >
+      <Typography variant="subtitle1" align="center" component="p">
+        <span className="font">Not a big talker? Drop a</span>{" "}
+        <IconButton>
           <ContactMe />
         </IconButton>
       </Typography>
-      <Typography
-        variant="body2"
-        align="center"
-        gutterBottom
-      >
-        <Icon
-          name='map marker alternate'
-          fitted
-        />  <span className='font'>Calgary-AB, Canada</span>
+      <Typography variant="body2" align="center" gutterBottom>
+        <Icon name="map marker alternate" fitted />{" "}
+        <span className="font">Calgary-AB </span>
+        <span className="leaf">
+          <FaCanadianMapleLeaf />
+        </span>
       </Typography>
-      <Typography
-        variant="body2"
-        align="center"
-        gutterBottom
-      >
-        <span className='font'>
-          {' © '}2021{" "}-{" "}{new Date().getFullYear()},
+      <Typography variant="body2" align="center" gutterBottom>
+        <span className="font">
+          {" © "}2021 - {new Date().getFullYear()},
           <span className="font-link"> My Projects</span>
         </span>
       </Typography>
-      <Typography
-        variant="body2"
-        align="center"
-        component="div"
-      >
+      <Typography variant="body2" align="center" component="div">
         <Today />
       </Typography>
     </footer>
-  )
+  );
 }
 
 export default Footer;
-
-
