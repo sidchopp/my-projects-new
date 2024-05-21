3061f9895bdaf0ad36f33f2537ce13ed56e37a9d
sidchopp <sidchopp@gmail.com>
2024-05-21 14:26:43 -0600

    FinTrack project added

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,5 +1,19 @@
 const ProjectData = [
   {
+    id: "19",
+    order: 19,
+    title: "Fin Track",
+    bestView: "On ALL Big Screens, Laptops, Tablets, Mobiles",
+    dates: "May 2024 - May 2024",
+    description: [
+      "Track every dollar effortlessly and stay stress-free about your money.",
+    ],
+    img: "https://images.unsplash.com/photo-1579621970588-a35d0e7ab9b6?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    website: "https://fintrack-8f7c3.web.app/login",
+    techUsed: "React, Firebase",
+    company: "SID",
+  },
+  {
     id: "18",
     order: 18,
     title: "Hotel Reservation",
